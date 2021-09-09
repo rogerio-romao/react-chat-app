aefb2e4f7b798c10459286d7892b43b87b6b27a5
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2021-09-09 17:49:33 +0100

    created public and private routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { Switch } from 'react-router';
+
+import 'rsuite/dist/styles/rsuite-default.css';
+import './styles/main.scss';
+
+import PrivateRoute from './components/PrivateRoute';
+import PublicRoute from './components/PublicRoute';
+
+import SignIn from './pages/SignIn';
+import Home from './pages/Home';
 
 function App() {
-  return <div>hello world</div>
+  return (
+    <Switch>
+      <PublicRoute path="/signin">
+        <SignIn />
+      </PublicRoute>
+      <PrivateRoute path="/">
+        <Home />
+      </PrivateRoute>
+    </Switch>
+  );
 }
 
-export default App
+export default App;
---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyDV5BDzQiPikQLpMPend_eXiLAS1jtuAV0',
+  authDomain: 'react-chat-app-fca7d.firebaseapp.com',
+  projectId: 'react-chat-app-fca7d',
+  storageBucket: 'react-chat-app-fca7d.appspot.com',
+  messagingSenderId: '382346123103',
+  appId: '1:382346123103:web:e38b0a6938ccdcba1b08ea',
+};
+
+const app = firebase.initializeApp(config);
