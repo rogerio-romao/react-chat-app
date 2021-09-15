a227be10ab230e42e91cd7b635629039f04cac0c
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2021-09-15 14:41:39 +0100

    added real time presence listener for users

--- src/context/profile.context.js
+++ src/context/profile.context.js
@@ -1,7 +1,19 @@
 import React, { createContext, useState, useContext, useEffect } from 'react';
+import firebase from 'firebase/compat/app';
+
 import { auth, database } from '../misc/firebase';
 
 const ProfileContext = createContext();
+
+export const isOfflineForDatabase = {
+  state: 'offline',
+  last_changed: firebase.database.ServerValue.TIMESTAMP,
+};
+
+const isOnlineForDatabase = {
+  state: 'online',
+  last_changed: firebase.database.ServerValue.TIMESTAMP,
+};
 
 export const ProfileProvider = ({ children }) => {
   const [profile, setProfile] = useState(null);
@@ -9,8 +21,10 @@ export const ProfileProvider = ({ children }) => {
 
   useEffect(() => {
     let userRef;
+    let userStatusRef;
     const authUnsub = auth.onAuthStateChanged(authObj => {
       if (authObj) {
+        userStatusRef = database.ref(`/status/${authObj.uid}`);
         userRef = database.ref(`/profiles/${authObj.uid}`);
         userRef.on('value', snap => {
           const { name, createdAt, avatar } = snap.val();
@@ -24,10 +38,26 @@ export const ProfileProvider = ({ children }) => {
           setProfile(profileData);
           setIsLoading(false);
         });
+
+        database.ref('.info/connected').on('value', snapshot => {
+          if (snapshot.val() === false) {
+            return;
+          }
+          userStatusRef
+            .onDisconnect()
+            .set(isOfflineForDatabase)
+            .then(() => {
+              userStatusRef.set(isOnlineForDatabase);
+            });
+        });
       } else {
         if (userRef) {
           userRef.off();
         }
+        if (userStatusRef) {
+          userStatusRef.off();
+        }
+        database.ref('.info/connected').off();
         setProfile(null);
         setIsLoading(false);
       }
@@ -35,8 +65,12 @@ export const ProfileProvider = ({ children }) => {
 
     return () => {
       authUnsub();
+      database.ref('.info/connected').off();
       if (userRef) {
         userRef.off();
+      }
+      if (userStatusRef) {
+        userStatusRef.off();
       }
     };
   }, []);
