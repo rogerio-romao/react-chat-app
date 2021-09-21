5cf262ac9832ff894e1445a9c1f56d978b9e60f8
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2021-09-22 00:17:26 +0100

    added global error boundary

--- src/App.js
+++ src/App.js
@@ -7,22 +7,25 @@ import './styles/main.scss';
 import { ProfileProvider } from './context/profile.context';
 import PrivateRoute from './components/PrivateRoute';
 import PublicRoute from './components/PublicRoute';
+import { ErrorBoundary } from './components/ErrorBoundary';
 
 import SignIn from './pages/SignIn';
 import Home from './pages/Home';
 
 function App() {
   return (
-    <ProfileProvider>
-      <Switch>
-        <PublicRoute path="/signin">
-          <SignIn />
-        </PublicRoute>
-        <PrivateRoute path="/">
-          <Home />
-        </PrivateRoute>
-      </Switch>
-    </ProfileProvider>
+    <ErrorBoundary>
+      <ProfileProvider>
+        <Switch>
+          <PublicRoute path="/signin">
+            <SignIn />
+          </PublicRoute>
+          <PrivateRoute path="/">
+            <Home />
+          </PrivateRoute>
+        </Switch>
+      </ProfileProvider>
+    </ErrorBoundary>
   );
 }
 
